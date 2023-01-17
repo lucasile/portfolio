b177ec0f9c7ea87d7a155f82732540af8bec5395
Lucas <lucas1.ilea@gmail.com>
2023-01-16 22:58:55 -0500

    Added front page and preloader

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      transitionProperty: {
+        multiple: "width , height",
+      },
+    },
+  },
+  plugins: [],
+};
